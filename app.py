31f877c57cbb37c9ee612619f6562e5fdf3b905d
Samuel Folledo <samuelfolledo@gmail.com>
2019-10-04 00:50:49 -0700

    added mongodb and user model

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
+from pymongo import MongoClient
 
 app = Flask(__name__)
+
+client = MongoClient()
+db = client.Contractor
+playlists = db.users
 
 @app.route('/')
 def index():
