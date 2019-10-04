90943ad2c59dc92ad9c9f7a6bfc23c7687b10e98
Samuel Folledo <samuelfolledo@gmail.com>
2019-10-04 10:12:01 -0700

    [add] users can edit and update users

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def users_index():
 
 @app.route('/users/new') #NEW
 def userss_new():
-    return render_template('users_new.html')
+    return render_template('users_new.html', user = {}, title = 'New User')
 
 @app.route('/users', methods=['POST']) #SUBMIT
 def users_submit():
@@ -31,6 +31,23 @@ def users_show(user_id):
     user = users.find_one({'_id': ObjectId(user_id)})
     return render_template('users_show.html', user = user)
 
+@app.route('/users/<user_id>/edit') #EDIT
+def users_edit(user_id):
+    user = users.find_one({'_id': ObjectId(user_id)})
+    return render_template('users_edit.html', user=user, title = 'Edit User')
+
+@app.route('/users/<user_id>', methods=['POST']) #Submit the UPDATED user
+def users_update(user_id):
+    updated_user = {
+        'user_name': request.form.get('user_name'),
+        'user_description': request.form.get('user_description'),
+        # 'videos': request.form.get('videos').split()
+        'user_rating': request.form.get('user_rating')
+    }
+    users.update_one(
+        {'_id': ObjectId(user_id)},
+        {'$set': updated_user})
+    return redirect(url_for('users_show', user_id=user_id))
 
 
 if __name__ == '__main__':
