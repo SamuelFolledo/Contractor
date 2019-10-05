e90bca54d3aa7455ee6deba8a402bd6cfabe48d9
Samuel Folledo <samuelfolledo@gmail.com>
2019-10-04 21:35:16 -0700

    [add] added necessary lines to make it live in Heroku

--- app.py
+++ app.py
@@ -1,12 +1,16 @@
 from flask import Flask, render_template, request, redirect, url_for
 from pymongo import MongoClient
 from bson.objectid import ObjectId
+import os
+from datetime import datetime
 
 app = Flask(__name__)
 
-client = MongoClient()
-db = client.Contractor
+host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Contractor')
+client = MongoClient(host = f'{host}?retryWrites=false')
+db = client.get_default_database()
 users = db.users
+
 
 @app.route('/')
 def users_index():
@@ -56,4 +60,4 @@ def users_delete(user_id):
 
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, host='0.0.0.0', port=os.environ.get('PORT', 5000))
