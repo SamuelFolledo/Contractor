a15b8ef4aa7dd096652f1d9602607d591c385bdc
Samuel Folledo <samuelfolledo@gmail.com>
2019-10-08 15:33:04 -0700

    [add] nav bar always works now.This is before adding user authentication

--- app.py
+++ app.py
@@ -10,23 +10,16 @@ host = os.environ.get('MONGODB_URI', 'mongodb://localhost:27017/Contractor')
 client = MongoClient(host = f'{host}?retryWrites=false')
 db = client.get_default_database()
 users = db.users
-current_users = db.current_users
 
+current_user = {"username":"", "_id":""}
 
 @app.route('/')
 def users_index():
-    user_id = current_users.find()
-    print(f"User id is {user_id}")
-    current_user = None
-    if user_id == None:
-        current_user = users.find_one({'_id': ObjectId(user_id)})
-        print(f"User id is {current_user}") 
-    
     return render_template('users_index.html', current_user = current_user, users = users.find())
 
 @app.route('/users/new') #NEW
-def userss_new():
-    return render_template('users_new.html', user = {}, title = 'New User')
+def users_new():
+    return render_template('users_new.html', current_user = current_user, user = {}, title = 'New User')
 
 @app.route('/users', methods=['POST']) #SUBMIT
 def users_submit():
@@ -40,17 +33,17 @@ def users_submit():
         'user_github': request.form.get('user_github')
     }
     user_id = users.insert_one(user).inserted_id
-    return redirect(url_for('users_show', user_id = user_id))
+    return redirect(url_for('users_show', current_user = current_user, user_id = user_id))
 
 @app.route('/users/<user_id>') #SHOW
 def users_show(user_id):
     user = users.find_one({'_id': ObjectId(user_id)})
-    return render_template('users_show.html', user = user)
+    return render_template('users_show.html', current_user = current_user, user = user)
 
 @app.route('/users/<user_id>/edit') #EDIT
 def users_edit(user_id):
     user = users.find_one({'_id': ObjectId(user_id)})
-    return render_template('users_edit.html', user=user, title = 'Edit User')
+    return render_template('users_edit.html', current_user = current_user, user=user, title = 'Edit User')
 
 @app.route('/users/<user_id>', methods=['POST']) #Submit the UPDATED user
 def users_update(user_id):
@@ -67,7 +60,7 @@ def users_update(user_id):
     users.update_one(
         {'_id': ObjectId(user_id)},
         {'$set': updated_user})
-    return redirect(url_for('users_show', user_id=user_id))
+    return redirect(url_for('users_show', current_user = current_user, user_id=user_id))
 
 @app.route('/users/<user_id>/delete', methods=['POST']) #DELETE
 def users_delete(user_id):
